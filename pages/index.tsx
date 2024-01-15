37670ef147124d976bf387ca28c44aacf4479b7f
Kohei Matsubara <matsubygh@gmail.com>
2024-01-15 14:09:08 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import { Link } from "@chakra-ui/next-js";
 export default function Home() {
   return (
     <>
-      <Box>NEXT-PREFETCH-TEST</Box>
+      <Box mb="200px">NEXT-PREFETCH-TEST</Box>
       <Link href="/test1">test1</Link>
       <Link href="/test2">test2</Link>
-      <Link href="/test3">test2</Link>
+      <Link href="/test3">test3</Link>
     </>
   );
 }
