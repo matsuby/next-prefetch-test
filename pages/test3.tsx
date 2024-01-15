22e84d9b994d5a4c647293a70b5dd7582191d359
Kohei Matsubara <matsubygh@gmail.com>
2024-01-15 13:46:12 +0900

    test

--- pages/test3.tsx
+++ pages/test3.tsx
@@ -1,10 +1,21 @@
 import { Box } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/next-js";
+import { Link, type LinkProps } from "@chakra-ui/next-js";
+import { useRouter } from "next/router";
 
 export default function Test3Page() {
   return (
-    <Box as={Link} href="/links/piyo" prefetch={false}>
+    <Box as={MyLink} href="/links/piyo">
       piyo
     </Box>
+  );
+}
+
+function MyLink({ children, href, ...props }: LinkProps) {
+  const { isPreview } = useRouter();
+
+  return (
+    <Link href={href} prefetch={!!isPreview} {...props}>
+      {children}
+    </Link>
   );
 }
